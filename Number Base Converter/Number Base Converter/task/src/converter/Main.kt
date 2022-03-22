bf6f2d7151e2f6edf2bf0705fd45479993dd2c18
Денис <mylcmail@mail.ru>
2022-03-23 00:19:20 +0300

    add: Number base converter - stage 3

--- Number Base Converter/Number Base Converter/task/src/converter/Main.kt
+++ Number Base Converter/Number Base Converter/task/src/converter/Main.kt
@@ -1,41 +1,27 @@
 package converter
 
+import java.math.BigInteger
+import java.util.Scanner
+
+const val BACK_COMMAND = "/back"
+const val EXIT_COMMAND = "/exit"
+val scanner = Scanner(System.`in`)
 fun main() {
     var isStopped = false
     while (!isStopped) {
-        println("Do you want to convert /from decimal or /to decimal? (To quit type /exit)")
-        when (readln()) {
-            "/from" -> convertFromDecimal()
-            "/to" -> convertToDecimal()
-            "/exit" -> isStopped = true
+        println("Enter two numbers in format: {source base} {target base} (To quit type /exit)")
+        val sourceBase = scanner.next()
+        if (sourceBase == EXIT_COMMAND) {
+            isStopped = true
+            continue
+        }
+        val targetBase = scanner.nextInt()
+        var number = ""
+        while (number != BACK_COMMAND) {
+            println("Enter number in base $sourceBase to convert to base $targetBase (To go back type /back)")
+            number = scanner.next()
+            if (number == BACK_COMMAND) continue
+            println("Conversion result: ${BigInteger(number, sourceBase.toInt()).toString(targetBase)}")
         }
     }
-
-
-}
-
-fun convertFromDecimal() {
-    println("Enter a number in decimal system:")
-    val decimalNumber = readln().toInt()
-    println("Enter the target base:")
-    val result = when (readln().toInt()) {
-        2 -> Integer.toBinaryString(decimalNumber)
-        8 -> Integer.toOctalString(decimalNumber)
-        16 -> Integer.toHexString(decimalNumber)
-        else -> "incorrect base"
-    }
-    println("Conversion result: $result")
-}
-
-fun convertToDecimal() {
-    println("Enter source number:")
-    val sourceNumber = readln()
-    println("Enter source base:")
-    val result = when (readln().toInt()) {
-        2 -> Integer.parseInt(sourceNumber, 2)
-        8 -> Integer.parseInt(sourceNumber, 8)
-        16 -> Integer.parseInt(sourceNumber, 16)
-        else -> "incorrect base"
-    }
-    println("Conversion to decimal result: $result")
 }
